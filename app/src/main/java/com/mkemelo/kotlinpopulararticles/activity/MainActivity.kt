62282065491e28202ea40e39c25e03396728bfbe
@mkemelo <mkemelo@bitri.co.bw>
2021-09-10 01:37:27 +0200

    Fixing graddle version issues after updating android studio

--- app/src/main/java/com/mkemelo/kotlinpopulararticles/activity/MainActivity.kt
+++ app/src/main/java/com/mkemelo/kotlinpopulararticles/activity/MainActivity.kt
@@ -4,27 +4,33 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.mkemelo.kotlinpopulararticles.R
 import com.mkemelo.kotlinpopulararticles.adapter.RecyclerViewAdapter
+import com.mkemelo.kotlinpopulararticles.databinding.ActivityMainBinding
 import com.mkemelo.kotlinpopulararticles.model.ArticlesListData
 import com.mkemelo.kotlinpopulararticles.viewmodels.MainActivityViewModel
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var recyclerViewAdapter: RecyclerViewAdapter
+
+    private lateinit var adapter: RecyclerViewAdapter
+    private lateinit var recyclerView: RecyclerView;
+    private lateinit var recyclerViewAdapter: RecyclerViewAdapter;
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val articlePeriods = resources.getStringArray(R.array.periods)
-        val spinner = findViewById<Spinner>(R.id.spinner)
+        val spinner = binding.spinner
         if (spinner != null) {
             val adapter = ArrayAdapter(this,
                 android.R.layout.simple_spinner_item, articlePeriods)
@@ -50,6 +56,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initRecyclerView() {
+        recyclerView = binding.recyclerView
         recyclerView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             recyclerViewAdapter = RecyclerViewAdapter()
@@ -62,7 +69,7 @@ class MainActivity : AppCompatActivity() {
     fun loadArticlesData(period: String) {
         val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         viewModel.getArticlesDataObserver().observe(this, Observer<ArticlesListData>{ response ->
-            println("Server resposnse ************************ $response")
+            // println("Server resposnse ************************ $response")
             if(response != null) {
                 recyclerViewAdapter.setListData(response.results)
                 recyclerViewAdapter.notifyDataSetChanged()
--- app/src/main/java/com/mkemelo/kotlinpopulararticles/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/mkemelo/kotlinpopulararticles/adapter/RecyclerViewAdapter.kt
@@ -11,7 +11,8 @@ import com.bumptech.glide.Glide
 import com.mkemelo.kotlinpopulararticles.ArticleData
 import com.mkemelo.kotlinpopulararticles.activity.DetailView
 import com.mkemelo.kotlinpopulararticles.R
-import kotlinx.android.synthetic.main.articles_list_item.view.*
+import com.mkemelo.kotlinpopulararticles.databinding.ArticlesListItemBinding
+import java.util.*
 
 class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
 
@@ -21,6 +22,7 @@ class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder
         this.items = data
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ArticlesListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         val inflater = LayoutInflater.from(parent.context).inflate(R.layout.articles_list_item, parent, false)
 
         return MyViewHolder(inflater)
@@ -42,51 +44,38 @@ class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder
             itemView.context.startActivity(intent)
         }
 
-        // Set values displayed each card
-        val tvTitle = view.tvTitle
-        val abstract = view.tvAbstract
-        val tvByline = view.tvByline
-        val imageThumb = view.imageThumb
-        val published = view.tvPublished
-
+        val binding = ArticlesListItemBinding.bind(view)
         fun bind(data: ArticleData) {
-            tvTitle.text = data.title
-            tvByline.text = data.byline
-            published.text = data.published_date
-            abstract.text = data.abstract
+            // Set values displayed each card
+            val tvTitle = binding.tvTitle
+            val abstract = binding.tvAbstract
+            val tvByline = binding.tvByline
+            val imageThumb = binding.imageThumb
+            val published = binding.tvPublished
+            with(binding) {
+                tvTitle.text = data.title
+                tvByline.text = data.byline
+                published.text = data.published_date
+                abstract.text = data.abstract
 
+                if (data?.media.isNotEmpty() && data?.media[0]?.metadata.isNotEmpty() && !TextUtils.isEmpty(data?.media[0]?.metadata[0].url)) {
+                    val img = data?.media[0]?.metadata[0]?.url ?: "No image"
+                    Glide.with(imageThumb)
+                        .load(img)
+                        .placeholder(R.drawable.thumbnail)
+                        .error(R.drawable.thumbnail)
+                        .fallback(R.drawable.thumbnail)
+                        .into(imageThumb)
 
+                } else {
+                    println("**************** NOTHING TO PRINT HERE");
+                    Glide.with(imageThumb)
+                        .load(R.drawable.thumbnail)
+                        .into(imageThumb)
+                }
 
-            if (data?.media.isNotEmpty() && data?.media[0]?.metadata.isNotEmpty() && !TextUtils.isEmpty(data?.media[0]?.metadata[0].url)) {
-                val img = data?.media[0]?.metadata[0]?.url ?: "No image"
-                Glide.with(imageThumb)
-                    .load(img)
-                    .placeholder(R.drawable.thumbnail)
-                    .error(R.drawable.thumbnail)
-                    .fallback(R.drawable.thumbnail)
-                    .into(imageThumb)
-
-            } else {
-                println("**************** NOTHING TO PRINT HERE");
-                Glide.with(imageThumb)
-                    .load(R.drawable.thumbnail)
-                    .into(imageThumb)
             }
-           /* if(!TextUtils.isEmpty(data?.media[0]?.metadata[0]?.url)) {
-                val img = data?.media[0]?.metadata[0]?.url
-                println("**************** $img");
 
-                *//*Glide.with(imageThumb)
-                    .load(data.media[0].metadata[0].url)
-                    .circleCrop()
-                    .placeholder(R.drawable.default_thumb)
-                    .error(R.drawable.default_thumb)
-                    .fallback(R.drawable.default_thumb)
-                    .into(imageThumb)*//*
-            } else {
-
-                println("**************** NOTHING TO SHOW HERE");
-            }*/
         }
 
         init {
